e3a286e002a22715f9a392e150ccf987404e168d
Reihn <reipae19@kingshigh.school.nz>
2025-06-12 14:57:06 +1200

    Script for paused menu

--- paused_menu.gd
+++ paused_menu.gd
@@ -1,11 +1,25 @@
 extends Control
 
+func resume():
+	get_tree(). paused = false 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func paused():
+	get_tree().paused = true 
+
+func testEsc():
+	if Input.is_action_just_pressed("esc") and !get_tree().paused:
+		paused()
+	elif Input.is_action_just_pressed("esc") and get_tree().paused:
+		resume()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_resume_pressed() -> void:
+	resume()
+
+
+func _on_restart_pressed() -> void:
+	get_tree().reload_current_scene()
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit_delete()
