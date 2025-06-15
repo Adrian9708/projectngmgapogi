5ef89c2ce18974eadb68683bc5fb353b5100e291
Reihn <reipae19@kingshigh.school.nz>
2025-06-16 09:40:46 +1200

    health bar script

--- Health_Bar.gd
+++ Health_Bar.gd
@@ -1,11 +1,12 @@
 extends ProgressBar
 
+@export var player: AnimationPlayer
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _redy():
+	player.healthChange.connect(update)
+	update()
+	 
+	
+func udaye():
+	value = player.currentHealth * 100 player.maxHealth
+	
